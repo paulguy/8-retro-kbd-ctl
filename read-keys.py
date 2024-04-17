ba7433540b23d3f0038391a790d60e15bab64332
Paul Guy <paulguy119@gmail.com>
2024-04-17 17:41:20 -0400

    Make read-keys.py output 1 packet per line.

--- read-keys.py
+++ read-keys.py
@@ -14,11 +14,10 @@ IN_ID = 84
 CMD_ENABLE_KEYMAP = (0x76, 0xa5)
 CMD_DISABLE_KEYMAP = (0x76, 0xff)
 
-def listen_callback(cb_data, report_id, direction, data):
-    print(f"{data[0]}:", end='')
-    for item in data[1:]:
-        print(f" {item:02X}", end='')
-    print("\r", end='')
+def listen_callback(last_report, report_id, direction, data):
+    if last_report[0] != (report_id, direction, data):
+        print(hid.decode_interrupt(report_id, direction, data))
+        last_report[0] = (report_id, direction, data)
 
 with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
     hid = get_hid_desc(fd)
@@ -30,8 +29,9 @@ with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
     print(hid.decode_interrupt(buf[0], Endpoint.ADDRESS_DIR_OUT, buf[1:]))
     os.write(fd, buf)
 
+    last_report = [None]
     try:
-        listen(fd, hid, in_reports, out_reports, -1, listen_callback, None)
+        listen(fd, hid, in_reports, out_reports, -1, listen_callback, last_report)
     except KeyboardInterrupt:
         buf = generate_report(out_reports, OUT_ID, CMD_DISABLE_KEYMAP)
         print(hid.decode_interrupt(buf[0], Endpoint.ADDRESS_DIR_OUT, buf[1:]))
