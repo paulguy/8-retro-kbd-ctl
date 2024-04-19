198b22b0be74ae7683309a6ebfcd2d00fe879c2a
Paul Guy <paulguy119@gmail.com>
2024-04-18 21:58:34 -0400

    Separate some common functions out to another file.
    A common abstraction for the HID device itself seems like it could be
    helpful.

---
+++ lib/hiddev.py
@@ -0,0 +1,231 @@
+import os
+import select
+import fcntl
+import ctypes
+import array
+import itertools
+
+import pyudev
+from ioctl_opt import IOR as _IOR
+from ioctl_opt import IOC as _IOC
+
+from .usb import HID, Endpoint
+from .util import bits_to_bytes
+
+# include/linux/hid.h
+HID_MAX_DESCRIPTOR_SIZE = 4096
+
+# include/linux/hidraw.h
+class hidraw_report_descriptor(ctypes.Structure):
+    _fields_ = [
+        ('size', ctypes.c_uint),
+        ('value', ctypes.c_ubyte * HID_MAX_DESCRIPTOR_SIZE),
+    ]
+
+class hidraw_devinfo(ctypes.Structure):
+    _fields_ = [
+        ('bustype', ctypes.c_uint),
+        ('vendor', ctypes.c_short),
+        ('product', ctypes.c_short),
+    ]
+
+# get report descriptor size
+HIDIOCGDESCSIZE = _IOR(ord('H'), 0x01, ctypes.c_int)
+# get report descriptor
+HIDIOCGRDESC = _IOR(ord('H'), 0x02, hidraw_report_descriptor)
+# get raw info
+HIDIOCGRAWINFO = _IOR(ord('H'), 0x03, hidraw_devinfo)
+# get raw name
+def HIDIOCGRAWNAME(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x04, length)
+# get physical address
+def HIDIOCGRAWPHYS(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x05, length)
+# send feature report
+def HIDIOCSFEATURE(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x06, length)
+# get feature report
+def HIDIOCGFEATURE(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x07, length)
+# get raw uniq ??
+def HIDIOCGRAWUNIQ(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x08, length)
+# send input report
+def HIDIOCSINPUT(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x09, length)
+# get input report
+def HIDIOCGINPUT(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0A, length)
+# send output report
+def HIDIOCSOUTPUT(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0B, length)
+# get output report
+def HIDIOCGOUTPUT(length):
+    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0C, length)
+
+def find_hidraw_by_ids(udev, vendor, product, interface):
+    for device in udev.list_devices(subsystem='hidraw'):
+        # up from hidraw to hid-generic, usb_interface up to usb_device
+        usbdev = device.parent.parent.parent
+        if 'ID_VENDOR_ID' in usbdev.properties and \
+           'ID_MODEL_ID' in usbdev.properties and \
+           usbdev.properties['ID_VENDOR_ID'] == vendor and \
+           usbdev.properties['ID_MODEL_ID'] == product:
+            # i don't know how reliable this will be in the future, but
+            # i could see no other way
+            usbinterface = device.parent.parent.properties['DEVPATH']
+            index = usbinterface.rindex(".")+1
+            if int(usbinterface[index:]) == interface:
+                return device
+    return None
+
+class HIDDEV:
+    def raise_report_id_exception(report_id, direction=None):
+        reports = self.all_reports
+        if direction == Endpoint.ADDRESS_DIR_OUT:
+            reports = self.out_reports
+        if direction == Endpoint.ADDRESS_DIR_IN:
+            reports = self.in_reports
+        liststr = ""
+        for report in reports.keys:
+            liststr += f" {report}"
+
+        raise IndexError(f"Invalid report ID {report_id}, see list command for output reports. Valid options: {liststr}")
+
+    def get_desc_from_device(self):
+        size = ctypes.c_uint()
+        fcntl.ioctl(self.fd, HIDIOCGDESCSIZE, size)
+
+        buf = hidraw_report_descriptor()
+        buf.size = size
+        fcntl.ioctl(self.fd, HIDIOCGRDESC, buf, True)
+
+        return array.array('B', buf.value[:size.value])
+
+    def get_hid_desc(self, cached):
+        desc = array.array('B')
+        fromfile = False
+
+        if cached:
+            try:
+                with open("hid_desc.bin", "rb") as descfile:
+                    descfile.seek(0, os.SEEK_END)
+                    size = descfile.tell()
+                    descfile.seek(0, os.SEEK_SET)
+                    desc.fromfile(descfile, size)
+                    fromfile = True
+            except FileNotFoundError:
+                pass
+
+        if self.fd is not None:
+            desc = self.get_desc_from_device()
+        else:
+            return
+
+        if not fromfile:
+            with open("hid_desc.bin", "wb") as descfile:
+                desc.tofile(descfile)
+
+        self.hid.decode_desc(desc)
+        self.have_desc = True
+
+    def generate_report(self, report_id, data):
+        # convert to bytes and add 1 for report ID
+        try:
+            bufsize = bits_to_bytes(self.all_reports[report_id].get_size()) + 1
+        except KeyError:
+            self.raise_report_id_exception(report_id)
+
+        buf = array.array('B', (report_id,))
+
+        buf.extend(data)
+        if len(buf) < bufsize:
+            buf.extend(itertools.repeat(0, bufsize - len(buf)))
+
+        return buf
+
+    def select(self):
+        return len(select.select((self.fd,), (), (), 1)[0]) > 0
+
+    def read(self):
+        size = os.readv(self.fd, (self.largest_buf,))
+        return self.largest_buf[:size]
+
+    def write(self, buf):
+        return os.write(self.fd, buf)
+
+    def listen(self, count=-1, callback=None, cb_data=None):
+        while count != 0:
+            self.select()
+            buf = ""
+            try:
+                buf = self.read()
+            except BlockingIOError:
+                pass
+            if len(buf) > 0:
+                report_id = buf[0]
+                if callback is None:
+                    print(self.decode(report_id, buf[1:]))
+                else:
+                    if not callback(cb_data, report_id, buf[1:]):
+                        return
+            if count > 0:
+                count -= 1
+
+    def get_reports(self, direction=None):
+        if direction == Endpoint.ADDRESS_DIR_OUT:
+            return self.out_reports
+        elif direction == Endpoint.ADDRESS_DIR_IN:
+            return self.in_reports
+        return self.all_reports
+
+    def get_report_direction(self, report_id):
+        if report_id in self.out_reports:
+            return Endpoint.ADDRESS_DIR_OUT
+        if report_id in self.in_reports:
+            return Endpoint.ADDRESS_DIR_IN
+        self.raise_report_id_exception(report_id)
+
+    def decode(self, report_id, data):
+        return self.hid.decode_interrupt(report_id, self.get_report_direction(report_id), data)
+
+    def __init__(self, vendor_id, product_id, interface_num, force_no_cache=False, try_no_open=False):
+        self.fd = None
+        self.hid = HID()
+
+        self.have_desc = False
+
+        if try_no_open:
+            # can't force disuse of cache
+            self.get_hid_desc(True)
+
+        # if cache loading failed, try to open
+        if not try_no_open or (try_no_open and not self.have_desc):
+            udev = pyudev.Context()
+            device = find_hidraw_by_ids(udev, vendor_id, product_id, interface_num)
+            self.fd = os.open(device.device_node, os.O_RDWR | os.O_NONBLOCK)
+            if force_no_cache:
+                self.get_hid_desc(False)
+            else:
+                self.get_hid_desc(True)
+
+        self.out_reports = self.hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
+        self.in_reports = self.hid.get_reports(Endpoint.ADDRESS_DIR_IN)
+        self.all_reports = self.out_reports.copy()
+        self.all_reports.update(self.in_reports)
+
+        largest = 0
+        for report in self.all_reports:
+            size = self.all_reports[report].get_size()
+            if size > largest:
+                largest = size
+        # +1 for report id
+        self.largest_buf = array.array('B', itertools.repeat(0, bits_to_bytes(largest)+1))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.fd is not None:
+            os.close(self.fd)
+        return False
--- send-cmd.py
+++ send-cmd.py
@@ -2,118 +2,15 @@
 
 import sys
 import os
-import select
-import fcntl
-import ctypes
 import array
-import itertools
 
-import pyudev
-from ioctl_opt import IOR as _IOR
-from ioctl_opt import IOC as _IOC
-
-from lib.usb import HID, Endpoint
-from lib.util import BIT_MASKS, bits_to_bytes
+from lib.usb import Endpoint
+from lib.util import BIT_MASKS
+from lib.hiddev import HIDDEV
 
 VENDOR_ID = "2dc8"
 PRODUCT_ID = "5200"
 INTERFACE_NUM = 2
-
-# include/linux/hid.h
-HID_MAX_DESCRIPTOR_SIZE = 4096
-
-# include/linux/hidraw.h
-class hidraw_report_descriptor(ctypes.Structure):
-    _fields_ = [
-        ('size', ctypes.c_uint),
-        ('value', ctypes.c_ubyte * HID_MAX_DESCRIPTOR_SIZE),
-    ]
-
-class hidraw_devinfo(ctypes.Structure):
-    _fields_ = [
-        ('bustype', ctypes.c_uint),
-        ('vendor', ctypes.c_short),
-        ('product', ctypes.c_short),
-    ]
-
-# get report descriptor size
-HIDIOCGDESCSIZE = _IOR(ord('H'), 0x01, ctypes.c_int)
-# get report descriptor
-HIDIOCGRDESC = _IOR(ord('H'), 0x02, hidraw_report_descriptor)
-# get raw info
-HIDIOCGRAWINFO = _IOR(ord('H'), 0x03, hidraw_devinfo)
-# get raw name
-def HIDIOCGRAWNAME(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x04, length)
-# get physical address
-def HIDIOCGRAWPHYS(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x05, length)
-# send feature report
-def HIDIOCSFEATURE(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x06, length)
-# get feature report
-def HIDIOCGFEATURE(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x07, length)
-# get raw uniq ??
-def HIDIOCGRAWUNIQ(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x08, length)
-# send input report
-def HIDIOCSINPUT(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x09, length)
-# get input report
-def HIDIOCGINPUT(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0A, length)
-# send output report
-def HIDIOCSOUTPUT(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0B, length)
-# get output report
-def HIDIOCGOUTPUT(length):
-    return _IOC(ioctl_opt.IRC_READ, ord('H'), 0x0C, length)
-
-def print_properties(device):
-    for prop in device.properties:
-        print(f"{prop}={device.properties[prop]}")
-
-def find_hidraw_by_ids(udev, vendor, product, interface):
-    for device in udev.list_devices(subsystem='hidraw'):
-        # up from hidraw to hid-generic, usb_interface up to usb_device
-        usbdev = device.parent.parent.parent
-        if 'ID_VENDOR_ID' in usbdev.properties and \
-           'ID_MODEL_ID' in usbdev.properties and \
-           usbdev.properties['ID_VENDOR_ID'] == vendor and \
-           usbdev.properties['ID_MODEL_ID'] == product:
-            # i don't know how reliable this will be in the future, but
-            # i could see no other way
-            usbinterface = device.parent.parent.properties['DEVPATH']
-            index = usbinterface.rindex(".")+1
-            if int(usbinterface[index:]) == interface:
-                return device
-    return None
-
-class HIDDEV:
-    def __init__(self, vendor_id, product_id, interface_num):
-        udev = pyudev.Context()
-
-        device = find_hidraw_by_ids(udev, vendor_id, product_id, interface_num)
-
-        self.fd = os.open(device.device_node, os.O_RDWR | os.O_NONBLOCK)
-
-    def __enter__(self):
-        return self.fd
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.close(self.fd)
-        return False
-
-def get_desc_from_device(fd):
-    size = ctypes.c_uint()
-    fcntl.ioctl(fd, HIDIOCGDESCSIZE, size)
-
-    buf = hidraw_report_descriptor()
-    buf.size = size
-    fcntl.ioctl(fd, HIDIOCGRDESC, buf, True)
-
-    return array.array('B', buf.value[:size.value])
 
 def decode_args(args):
     vals = array.array('B')
@@ -150,30 +47,7 @@ def decode_args(args):
                 bit %= 8
     return num, vals, -1
 
-def get_largest_report(reports):
-    largest = 0
-    for report in reports:
-        size = reports[report].get_size()
-        if size > largest:
-            largest = size
-    return largest
-
-def generate_report(reports, report_id, data):
-    # convert to bytes and add 1 for report ID
-    try:
-        bufsize = bits_to_bytes(reports[report_id].get_size()) + 1
-    except KeyError:
-        raise IndexError(f"Invalid report ID {report_id}, see list command for output reports.")
-
-    buf = array.array('B', (report_id,))
-
-    buf.extend(data)
-    if len(buf) < bufsize:
-        buf.extend(itertools.repeat(0, bufsize - len(buf)))
-
-    return buf
-
-def generate_reports(reports, args):
+def generate_reports(hid, args):
     bufs = []
     pos = 0
     while pos < len(args):
@@ -182,66 +56,9 @@ def generate_reports(reports, args):
         num, vals, count = decode_args(args[pos+1:])
         pos += num + 1
 
-        bufs.append((generate_report(reports, report_id, vals), count))
+        bufs.append((hid.generate_report(report_id, vals), count))
 
     return bufs
-
-def listen(fd, hid, in_reports, out_reports, count=-1, callback=None, cb_data=None):
-    largest = get_largest_report(in_reports)
-    out_largest = get_largest_report(out_reports)
-    if out_largest > largest:
-        largest = out_largest
-    largest = bits_to_bytes(largest) + 1
-
-    buf = array.array('B', itertools.repeat(0, largest))
-
-    while count != 0:
-        select.select((fd,), (), (), 1)
-        data_read = 0
-        try:
-            data_read = os.readv(fd, (buf,))
-        except BlockingIOError:
-            pass
-        if data_read > 0:
-            report_id = buf[0]
-            direction = Endpoint.ADDRESS_DIR_OUT
-            if report_id in in_reports:
-                direction = Endpoint.ADDRESS_DIR_IN
-            if callback is None:
-                print(hid.decode_interrupt(report_id, direction, buf[1:]))
-            else:
-                callback(cb_data, report_id, direction, buf[1:])
-        if count > 0:
-            count -= 1
-
-def get_hid_desc(fd=-1, cached=True):
-    desc = array.array('B')
-    fromfile = False
-
-    if cached:
-        try:
-            with open("hid_desc.bin", "rb") as descfile:
-                descfile.seek(0, os.SEEK_END)
-                size = descfile.tell()
-                descfile.seek(0, os.SEEK_SET)
-                desc.fromfile(descfile, size)
-                fromfile = True
-        except FileNotFoundError:
-            pass
-
-    if len(desc) == 0:
-        if fd < 0:
-            return None
-        desc = get_desc_from_device(fd)
-
-    if not fromfile:
-        with open("hid_desc.bin", "wb") as descfile:
-            desc.tofile(descfile)
-
-    hid = HID()
-    hid.decode_desc(desc)
-
-    return hid
 
 def usage():
     print(f"USAGE: {sys.argv[0]} <list|decode-raw <sequence>|send-raw <sequence>|listen>\n\n"
@@ -261,11 +78,10 @@ def usage():
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "list":
-            with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
-                hid = get_hid_desc(fd, cached=False)
+            with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM, force_no_cache=True) as hid:
+                out_reports = hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
+                in_reports = hid.get_reports(Endpoint.ADDRESS_DIR_IN)
 
-            out_reports = hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
-            in_reports = hid.get_reports(Endpoint.ADDRESS_DIR_IN)
             print("Out Reports")
             for report in out_reports.keys():
                 print(f"{report}: {out_reports[report].get_size()}bit {out_reports[report]}")
@@ -274,50 +90,31 @@ if __name__ == '__main__':
                 print(f"{report}: {in_reports[report].get_size()}bit {in_reports[report]}")
         elif sys.argv[1] == "decode-raw":
             if len(sys.argv) > 2:
-                # try to get the cached value first, otherwise try to get it from the device
-                hid = get_hid_desc(-1)
-                if hid is None:
-                    with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
-                        hid = get_hid_desc(fd)
+                with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM, try_no_open=True) as hid:
+                    bufs = generate_reports(hid, sys.argv[2:])
 
-                reports = hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
-
-                bufs = generate_reports(reports, sys.argv[2:])
-
-                for buf in bufs:
-                    print(hid.decode_interrupt(buf[0][0], Endpoint.ADDRESS_DIR_OUT, buf[0][1:]))
-                    if buf[1] < 0:
-                        print("Listen forever")
-                    elif buf[1] == 0:
-                        print("Don't listen")
-                    else:
-                        print(f"Listen for {buf[1]} packets")
+                    for buf in bufs:
+                        print(hid.decode(buf[0][0], buf[0][1:]))
+                        if buf[1] < 0:
+                            print("Listen forever")
+                        elif buf[1] == 0:
+                            print("Don't listen")
+                        else:
+                            print(f"Listen for {buf[1]} packets")
             else:
                 usage()
         elif sys.argv[1] == "listen":
-            with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
-                hid = get_hid_desc(fd)
-
-                in_reports = hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
-                out_reports = hid.get_reports(Endpoint.ADDRESS_DIR_IN)
-
-                listen(fd, hid, in_reports, out_reports)
+            with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as hid:
+                hid.listen()
         elif sys.argv[1] == "send-raw":
             if len(sys.argv) > 2:
-                with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as fd:
-                    hid = get_hid_desc(fd)
-
-                    in_reports = hid.get_reports(Endpoint.ADDRESS_DIR_IN)
-                    out_reports = hid.get_reports(Endpoint.ADDRESS_DIR_OUT)
-
-                    bufs = generate_reports(out_reports, sys.argv[2:])
+                with HIDDEV(VENDOR_ID, PRODUCT_ID, INTERFACE_NUM) as hid:
+                    bufs = generate_reports(hid, sys.argv[2:])
 
                     for buf in bufs:
-                        print(hid.decode_interrupt(buf[0][0], Endpoint.ADDRESS_DIR_OUT, buf[0][1:]))
-
-                        os.write(fd, buf[0])
-
-                        listen(fd, hid, in_reports, out_reports, buf[1])
+                        print(hid.decode(buf[0][0], buf[0][1:]))
+                        hid.write(buf[0])
+                        hid.listen(count=buf[1])
             else:
                 usage()
         else:
